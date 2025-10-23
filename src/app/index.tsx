800a8a0b6ad82bde72c7b09a4f0efc316e6a301f
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-10-23 13:01:51 +0530

    react-native

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import TextComponent from '../components/TextComponent'
+import ViewComponent from '../components/ViewComponent'
+
+const index = () => {
+  return (
+    <View>
+      <Text>
+
+
+
+        <TextComponent/>
+        <ViewComponent/>
+
+
+
+
+      </Text>
+    </View>
+  )
+}
+
+export default index
---
+++ src/components/ViewComponent.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ViewComponent = () => {
+  return (
+    <View>
+        <h1>Hii, this is Nilesh Kumar</h1>
+      <Text>ViewComponent</Text>
+      <button>Press Me</button>
+      
+    </View>
+  )
+}
+
+export default ViewComponent
